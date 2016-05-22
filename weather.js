33ef7ea06622092c4e4454743c8a898946125979
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-05-22 13:33:31 -0700

    Able to receive response data from server based on lat and long coords

--- weather.js
+++ weather.js
@@ -20,11 +20,30 @@ function locSuccess(position) {
     console.log(latitude);
     longitude = position.coords.longitude;
     console.log(longitude);
+    getWeather();
 }
 
 function locFail(msg) {
     console.log(msg.code);
 }
+
+
+var weatherKey = '05f04e04a675ecee84735203175cede4';
+var globalResponse;
+
+
+function getWeather() {
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=' + weatherKey,
+        method: 'POST',
+        success: function(result) {
+            console.log('in success function');
+            console.log(result);
+            globalResponse = result;
+        }
+    });
+}
+
 
 
 $(document).ready(function() {
