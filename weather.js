b7542f41f9f1a8483fd40dc6c27047dcbea482f3
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-05-22 14:17:39 -0700

    Added reverse goecoding using lat and long in order to get more specific location details for displaying

--- weather.js
+++ weather.js
@@ -20,6 +20,7 @@ function locSuccess(position) {
     console.log(latitude);
     longitude = position.coords.longitude;
     console.log(longitude);
+    getCoordsDetails();
     getWeather();
 }
 
@@ -28,18 +29,34 @@ function locFail(msg) {
 }
 
 
-var weatherKey = '05f04e04a675ecee84735203175cede4';
-var globalResponse;
+
+function displayWeather(data) {
+
+}
+
+
+function getCoordsDetails() {
+    var mapsKey = 'AIzaSyBGhh5LPrtzsHP-WKreyAU6aLlr0mMtXcg';
+    $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=' + mapsKey,
+        method: 'POST',
+        success: function(response) {
+            console.log('in get coords details success function', response);
+            //displayLocation(response);
+        }
+    });
+}
+
 
 
 function getWeather() {
+    var weatherKey = '05f04e04a675ecee84735203175cede4';
     $.ajax({
         url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=' + weatherKey,
         method: 'POST',
-        success: function(result) {
-            console.log('in success function');
-            console.log(result);
-            globalResponse = result;
+        success: function(response) {
+            console.log('in success function', response);
+            displayWeather(response);
         }
     });
 }
